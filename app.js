58e00be84baee52eb7b278ccf6a155dab8da3657
Max You <max.you15@gmail.com>
2013-12-06 11:08:27 -0800

    enable showing fake druid results

--- app.js
+++ app.js
@@ -19,9 +19,73 @@ if (env === 'development') {
 
 app.post('/search', function(req, res) {
   console.log('qs: ' + JSON.stringify(req.query));
-  res.send(200, []);
+  var events = druidResultToEvents(mockDruidResult);
+  res.send(200, {
+    events: events
+  });
 });
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+var druidResultToEvents = function(druidResult) {
+  flattenedEvents = [];
+  druidResult.forEach(function(bucket) {
+    bucketEvents = bucket.result.events;
+    bucketEvents.forEach(function(event) {
+      flattenedEvents.push(event.event);
+    });
+  });
+  return flattenedEvents;
+};
+
+var mockDruidResult = [{
+  "timestamp" : "2013-01-01T07:00:00.000Z",
+  "result" : {
+    "pagingIdentifiers" : {
+      "wikipedia_editstream_2012-12-29T00:00:00.000Z_2013-01-10T08:00:00.000Z_2013-01-10T08:13:47.830Z_v9" : 4
+    },
+    "events" : [ {
+      "segmentId" : "wikipedia_editstream_2012-12-29T00:00:00.000Z_2013-01-10T08:00:00.000Z_2013-01-10T08:13:47.830Z_v9",
+      "offset" : 0,
+      "event" : {
+        "timestamp" : "2013-01-01T07:00:00.000Z",
+        "namespace" : "article",
+        "added" : 22.0
+      }
+    }, {
+      "segmentId" : "wikipedia_editstream_2012-12-29T00:00:00.000Z_2013-01-10T08:00:00.000Z_2013-01-10T08:13:47.830Z_v9",
+      "offset" : 1,
+      "event" : {
+        "timestamp" : "2013-01-01T07:00:00.000Z",
+        "namespace" : "article",
+        "added" : 5.0
+      }
+    }, {
+      "segmentId" : "wikipedia_editstream_2012-12-29T00:00:00.000Z_2013-01-10T08:00:00.000Z_2013-01-10T08:13:47.830Z_v9",
+      "offset" : 2,
+      "event" : {
+        "timestamp" : "2013-01-01T07:00:00.000Z",
+        "namespace" : "article",
+        "added" : 5.0
+      }
+    }, {
+      "segmentId" : "wikipedia_editstream_2012-12-29T00:00:00.000Z_2013-01-10T08:00:00.000Z_2013-01-10T08:13:47.830Z_v9",
+      "offset" : 3,
+      "event" : {
+        "timestamp" : "2013-01-01T07:00:00.000Z",
+        "namespace" : "article",
+        "added" : 4.0
+      }
+    }, {
+      "segmentId" : "wikipedia_editstream_2012-12-29T00:00:00.000Z_2013-01-10T08:00:00.000Z_2013-01-10T08:13:47.830Z_v9",
+      "offset" : 4,
+      "event" : {
+        "timestamp" : "2013-01-01T07:00:00.000Z",
+        "namespace" : "article",
+        "added" : 4.0
+      }
+    } ]
+  }
+}];
